d01fcef02060c9f84df1e8ac03633fb234d4c060
DAYFA1R <im@dayfair.ca>
2025-11-10 00:31:03 -0500

    stubbing project structure and gitignore

---
+++ src/__init__.py
@@ -0,0 +1,1 @@
+# Creative Automation Pipeline for Social Campaigns
---
+++ src/models/__init__.py
@@ -0,0 +1,3 @@
+from .campaign import CampaignBrief, Product, AspectRatio
+
+__all__ = ['CampaignBrief', 'Product', 'AspectRatio']
